1abc56aa0ccfd217a51ecf82f26a48bef6b445c1
francescobernardi <fbernardi@inrete.it>
2018-04-27 13:03:27 +0200

    install bootstrap

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -44,7 +44,10 @@ class ArticlesController < ApplicationController
     flash[:notice] = "Article was successfully deleted"
     redirect_to articles_path
   end
+desc "Task description"
+task :task_name => [:dependent, :tasks] do
 
+end
   private
 
     def set_article
