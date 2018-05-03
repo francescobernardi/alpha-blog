8fd72be227c9928213a8079455764b1272c18d68
francescobernardi <fbernardi@inrete.it>
2018-05-03 18:50:22 +0200

    Complete authentication system

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   resources :articles
 
   get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to:'sessions#destroy'
   #post 'users', to: 'users#create'
   resources :users, except: [:new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
